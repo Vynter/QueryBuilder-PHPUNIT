03d968f766b9de70c5c24b133564ab1fa791ef95
Vynter <amine-cheraitia@hotmail.com>
2020-07-15 18:29:51 +0200

    first

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\QueryBuilder;
+
+require 'vendor/autoload.php';
+
+$int = new QueryBuilder();
+$z = $int->from('users', 'usr')->orderBy('name', 'DESC')->orderBy('id', 'ASC')->limit(5)->toSQL(); //pour appelé une function dans une autre sur la meme ligne il faut que la premier retourne l'instance en elle meme(return $this);
+var_dump($z);
